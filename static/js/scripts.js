147d2f86c5208346bba82587691148b4c889b48c
Gonzalo Ruiz Quispe <gonzaloruizquispe@gmail.com>
2024-08-06 08:02:06 -0300

    buttons home

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -49,18 +49,3 @@ function checkPasswordsMatch() {
 
     errorElement.textContent = password !== passwordConfirm ? 'Las contraseñas no coinciden.' : '';
 }
-
-let lastScrollTop = 0;
-const navbar = document.querySelector('.navbar');
-
-window.addEventListener('scroll', function() {
-    let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-
-    if (currentScroll > lastScrollTop) {
-        navbar.style.top = '-60px';
-    } else {
-        navbar.style.top = '0';
-    }
-
-    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
-});
